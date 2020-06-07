1ae7972448fb5d69787d43c3e97a2599a99be3fc
John <jpc.clancy@gmail.com>
2020-06-07 16:18:56 -0500

    Updates

---
+++ precache-manifest.e78b2c18ee9e2183b83378e41881c7a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25329a17cbfa64e502127e0bb970bc2c",
+    "url": "/iss-web-api/index.html"
+  },
+  {
+    "revision": "32907e8c32453d0a02c3",
+    "url": "/iss-web-api/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6ab1a9d5b9c0d2b57f5",
+    "url": "/iss-web-api/static/js/2.1b06fbbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iss-web-api/static/js/2.1b06fbbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32907e8c32453d0a02c3",
+    "url": "/iss-web-api/static/js/main.420f74e2.chunk.js"
+  },
+  {
+    "revision": "8f3d046ebd2fcd7c9d2d",
+    "url": "/iss-web-api/static/js/runtime-main.6cf074eb.js"
+  }
+]);
